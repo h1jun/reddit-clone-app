0ff29239dd3de26d8c233c42fed6202bb5ca77e4
Wonjun <dittoavec@gmail.com>
2022-09-26 16:23:04 +0900

    Add Vote entity

---
+++ server/src/entities/Vote.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import BaseEntity from "./Entity";
+import Post from "./Post";
+import { User } from "./User";
+
+// post와 comment에 대한 vote
+// comment에 대한 vote면 postId는 null
+// postId에 대한 vote면 commentId는 null
+@Entity("votes")
+export default class Vote extends BaseEntity {
+  @Column()
+  value: number; // 1 아니면 -1
+
+  // 투표를 한 유저
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "username", referencedColumnName: "username" })
+  user: User;
+
+  @Column()
+  usename: string;
+
+  @Column({ nullable: true })
+  postId: number;
+
+  @ManyToOne(() => Post)
+  post: Post;
+
+  @Column({ nullable: true })
+  commentId: number;
+
+  @ManyToOne(() => Comment)
+  comment: Comment;
+}

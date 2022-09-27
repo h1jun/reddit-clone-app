8871464fcfe80cb2ac66b27129ed96d80619a5e2
Wonjun <dittoavec@gmail.com>
2022-09-27 17:00:48 +0900

    Add 회원가입 요청 전송(client)

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Axios from "axios";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,13 +1,59 @@
 import Link from "next/link";
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import InputGroup from "../components/inputGroup";
+import axios from "axios";
+import { useRouter } from "next/router";
 
-const register = () => {
+const Register = () => {
+  const [email, setEmail] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errors, setErrors] = useState<any>({});
+
+  let router = useRouter();
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const res = await axios.post("/auth/register", {
+        // key와 value과 동일하면 생략 가능(email: email)
+        email,
+        username,
+        password,
+      });
+
+      console.log("res", res);
+      router.push("/login"); //login 페이지로 이동
+    } catch (error: any) {
+      console.log("error", error);
+      setErrors(error.response.data || {});
+    }
+  };
   return (
     <div className="bg-white">
       <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
           <h1 className="mb-2 text-lg font-medium">회원 가입</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
+            <InputGroup
+              placeholder="Email"
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
+            <InputGroup
+              placeholder="Username"
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder="password"
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
             <button className="w-full py-2 mb-1 text-cs font-bold text-white uppercaser bg-gray-400 border-gray-400 rounded">
               회원 가입
             </button>
@@ -24,4 +70,4 @@ const register = () => {
   );
 };
 
-export default register;
+export default Register;

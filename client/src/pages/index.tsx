3c750accfcc36ef8175aafbcf98236c53aa5e3c6
Wonjun <dittoavec@gmail.com>
2022-10-26 16:03:14 +0900

    add: 리스트 UI 생성

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+      {/* 포스트 리스트 */}
+      <div className="w-full md:mr-3 md:w-8/12"></div>
+
+      {/* 사이드바 */}
+      <div className="hidden w-4/12 ml-3 md:block">
+        <div className="bg-white border rounded">
+          <div className="p-4 border-b">
+            <p className="text-lg font-semibold text-center">상위 커뮤니티</p>
+          </div>
+
+          {/* 커뮤니티 리스트 */}
+          <div></div>
+
+          <div className="w-full py-6 text-center">
+            <Link href="/subs/create">
+              <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
+                커뮤니티 만들기
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

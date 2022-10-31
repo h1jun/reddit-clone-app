30b8e9a6612bde395e6c31cbba6b238b33e89065
Wonjun <dittoavec@gmail.com>
2022-10-31 18:16:17 +0900

    [server] add: 이미지 업로드 라우터 생성하기1

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -6,6 +6,10 @@ import { AppDataSource } from "../data-source";
 import Sub from "../entities/Sub";
 import { User } from "../entities/User";
 import Post from "../entities/Post";
+import { unlinkSync } from "fs";
+import path from "path";
+import multer, { FileFilterCallback } from "multer";
+import { makeid } from "../utils/helpers";
 
 const getSub = async (req: Request, res: Response) => {
   const name: string = req.params.name;
@@ -89,11 +93,103 @@ const topSubs = async (req: Request, res: Response) => {
   }
 };
 
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+
+  try {
+    const sub = await Sub.findOneOrFail({ where: { name: req.params.name } });
+
+    if (sub.username !== user.username) {
+      return res
+        .status(403)
+        .json({ error: "이 커뮤니티를 소유하고 있지 않습니다." });
+    }
+
+    res.locals.sub = sub;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "문제가 발생했습니다." });
+  }
+};
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "public/images",
+    filename: (_, file, callback) => {
+      const name = makeid(10);
+      callback(null, name + path.extname(file.originalname));
+    },
+  }),
+  fileFilter: (_, file: any, callback: FileFilterCallback) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      callback(null, true);
+    } else {
+      callback(new Error("이미지가 아닙니다."));
+    }
+  },
+});
+
+const uploadSubImage = async (req: Request, res: Response) => {
+  const sub: Sub = res.locals.sub;
+  try {
+    const type = req.body.type;
+    // 파일 유형을 지정치 않았을 시에는 업로든 된 파일 삭제
+    if (type !== "image" && type !== "banner") {
+      if (!req.file?.path) {
+        return res.status(400).json({ error: "유효하지 않은 파일" });
+      }
+
+      // 파일을 지워주기
+      unlinkSync(req.file.path);
+      return res.status(400).json({ error: "잘못된 유형" });
+    }
+
+    let oldImageUrn: string = "";
+
+    if (type === "image") {
+      // 사용중인 Urn 을 저장합니다. (이전 파일을 아래서 삭제하기 위해서)
+      oldImageUrn = sub.imageUrn || "";
+      // 새로운 파일 이름을 Urn 으로 넣어줍니다.
+      sub.imageUrn = req.file?.filename || "";
+    } else if (type === "banner") {
+      oldImageUrn = sub.bannerUrn || "";
+      sub.bannerUrn = req.file?.filename || "";
+    }
+    await sub.save();
+
+    // 사용하지 않는 이미지 파일 삭제
+    if (oldImageUrn !== "") {
+      const fullFilename = path.resolve(
+        process.cwd(),
+        "public",
+        "images",
+        oldImageUrn
+      );
+      unlinkSync(fullFilename);
+    }
+
+    return res.json(sub);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "문제가 발생했습니다." });
+  }
+};
+
 const router = Router();
 // 미들웨어 연결
 // userMiddleware -> authMiddleware -> createSub 순으로 실행
 router.get("/:name", userMiddleware, authMiddleware, getSub);
 router.post("/", userMiddleware, authMiddleware, createSub);
 router.get("/sub/topSubs", topSubs);
+router.post(
+  "/:name/upload",
+  userMiddleware,
+  authMiddleware,
+  ownSub,
+  upload.single("file"),
+  uploadSubImage
+);
 
 export default router;
